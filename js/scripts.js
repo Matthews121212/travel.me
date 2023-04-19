ff3383225268686131a753c206e6c2d62029cc50
Leonardo-DGS <17832817+Leonardo-DGS@users.noreply.github.com>
2023-04-19 02:11:57 +0200

    Make work increment/decrement days buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-/*!
-* Start Bootstrap - Full Width Pics v5.0.6 (https://startbootstrap.com/template/full-width-pics)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function addDays(days) {
+    let inputField = $("#quantity");
+    let newDays = parseInt(inputField.val()) + days;
+    if(newDays < 1) newDays = 1;
+    inputField.val(newDays);
+}
