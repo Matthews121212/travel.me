6654c993cc315c91f93bc5c1d0b364e5488779eb
alebianco99 <you@example.com>
2023-04-20 19:38:08 +0200

    fix

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                         <form class="d-inline-flex mx-1" role="search">
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" required>
                             <button onclick="addDays(-1)" class="btn-secondary btn mx-1" type="button"> <span class="material-symbols-outlined"> do_not_disturb_on </span> </button>
-                            <input type="text" pattern="[0-9]+" value="1" placeholder="Days" id="quantity" name="quantity" class="form-control mx-1"  required>
+                            <input type="text" pattern="[0-9]+" oninput="this.value = this.value.replace(/[^0-9]/g, '')" value="1" placeholder="Days" id="quantity" name="quantity" class="form-control mx-1"  required>
                             <button onclick="addDays(1)" class="btn-secondary btn mx-1" type="button"> <span class="material-symbols-outlined"> add_circle </span> </button>
                             <button class="btn-primary btn mx-1 " type="submit">Search</button>
                             <button class="btn-primary btn mx-1" type="submit">Find</button>
@@ -48,9 +48,9 @@
                                 </thead>
                                 <tbody>
                                   <tr>
-                                        <td>Big Ben and Westminster</td>
-                                        <td>Big Ben and Westminster</td>
-                                        <td>Big Ben and Westminster</td>
+                                    <td>Big Ben and Westminster</td>
+                                    <td>Big Ben and Westminster</td>
+                                    <td>Big Ben and Westminster</td>
                                   </tr>
                                   <tr>
                                     <td>Westminster Abbey</td>
@@ -79,25 +79,95 @@
                 </li>
 
                 <li class="list-group-item">
-                    <div class="container-fluid container-itinerario pt-2 pb-5" style="background-image: url('https://www.spagna.info/wp-content/uploads/sites/39/barcellona.jpg');">
-                        <div class="row text-bg-primary bg-gradient fw-bold fs-1"> Barcelona</div>
+                    <div class="container-fluid container-itinerario " style="background-image: url('https://www.spagna.info/wp-content/uploads/sites/39/barcellona.jpg');">  
+                        <div class="row text-bg-primary text-white">
+                            <div class="bg-gradient fw-bold fs-1">Barcelona</div>
+                            <table class="table text-white">
+                                <thead>
+                                  <tr>
+                                    <th scope="col">Day 1</th>
+                                    <th scope="col">Day 2</th>
+                                    <th scope="col">Day 3</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td>Big Ben and Westminster</td>
+                                    <td>Big Ben and Westminster</td>
+                                    <td>Big Ben and Westminster</td>
+                                  </tr>
+                                  <tr>
+                                    <td>Westminster Abbey</td>
+                                    <td>Westminster Abbey</td>
+                                    <td>Westminster Abbey</td>
+                                  </tr>
+                                  <tr>
+                                    <td>Buckingham Palace e Cambio della Guardia</td>
+                                    <td>Buckingham Palace e Cambio della Guardia</td>
+                                    <td>Buckingham Palace e Cambio della Guardia</td>
+                                  </tr>
+                                  <tr>
+                                    <td>Trafalgar Square</td>
+                                    <td>Trafalgar Square</td>
+                                    <td>Trafalgar Square</td>
+                                  </tr>
+                                  <tr>
+                                    <td>British Museum o National Gallery</td>
+                                    <td>British Museum o National Gallery</td>
+                                    <td>British Museum o National Gallery</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                        </div>
                     </div>
                 </li>
 
                 <li class="list-group-item">
-                    <div class="container-fluid container-itinerario pt-2 pb-5" style="background-image: url('https://www.franciaturismo.net/wp-content/uploads/sites/4/parigi-hd.jpg');">
-                        <div class="row text-bg-primary bg-gradient fw-bold fs-1"> Paris</div>
+                    <div class="container-fluid container-itinerario " style="background-image: url('https://www.franciaturismo.net/wp-content/uploads/sites/4/parigi-hd.jpg');">  
+                        <div class="row text-bg-primary text-white">
+                            <div class="bg-gradient fw-bold fs-1">Paris</div>
+                            <table class="table text-white">
+                                <thead>
+                                  <tr>
+                                    <th scope="col">Day 1</th>
+                                    <th scope="col">Day 2</th>
+                                    <th scope="col">Day 3</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td>Big Ben and Westminster</td>
+                                    <td>Big Ben and Westminster</td>
+                                    <td>Big Ben and Westminster</td>
+                                  </tr>
+                                  <tr>
+                                    <td>Westminster Abbey</td>
+                                    <td>Westminster Abbey</td>
+                                    <td>Westminster Abbey</td>
+                                  </tr>
+                                  <tr>
+                                    <td>Buckingham Palace e Cambio della Guardia</td>
+                                    <td>Buckingham Palace e Cambio della Guardia</td>
+                                    <td>Buckingham Palace e Cambio della Guardia</td>
+                                  </tr>
+                                  <tr>
+                                    <td>Trafalgar Square</td>
+                                    <td>Trafalgar Square</td>
+                                    <td>Trafalgar Square</td>
+                                  </tr>
+                                  <tr>
+                                    <td>British Museum o National Gallery</td>
+                                    <td>British Museum o National Gallery</td>
+                                    <td>British Museum o National Gallery</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                        </div>
                     </div>
                 </li>
             </ul>
         </section>
-
-        <!-- Image element - set the background image for the header in the line below-->
-        <!--div class="py-5 bg-image-full" style="background-image: url('https://source.unsplash.com/4ulffa6qoKA/1200x800')"-->
-            <!-- Put anything you want here! The spacer below with inline CSS is just for demo purposes!-->
-            <!--div style="height: 20rem"></div-->
-        <!--/div-->
-
+        
         <!-- Content section-->
         <section class="py-5">
             <div class="container my-5">
