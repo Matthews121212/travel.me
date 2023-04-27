722c369bd45ff8c6d5242c6b414258ea6103b63c
alebianco99 <you@example.com>
2023-04-27 19:24:04 +0200

    fix map

--- search.php
+++ search.php
@@ -8,9 +8,7 @@
     <body onload=initializeMap()>
         <?php include_once "assets/navbar.html" ?>
 
-        <div class="map-wrap">
         <div id="map"></div>
-        </div>
 
     <?php include_once "assets/footer.html" ?>
     
