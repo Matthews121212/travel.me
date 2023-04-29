ac56e8239179a6452e2d0d1ac370cb200a7e218b
Matteo Rossi <rossi.99.matteo@gmail.com>
2023-04-29 18:54:09 +0200

    Update Search page

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@ function addDays(days) {
 
 // Create map
 var map = L.map('map').setView([41.8902338,12.4907832], 13);
+let searchList  = new Array();
+var elementList 
 
 // Map layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -20,7 +22,20 @@ var foro = L.marker([41.8903461,12.4895719]).addTo(map)
 
 // per cancellarlo basta fare foro.remove()
 
-$( function() {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
-  } );
+function findPlace(){
+    if(document.getElementById("search-place").value.length <=3)
+        return;
+
+    $(".list-search-element").remove();
+
+    const place = encodeURI(document.getElementById("search-place").value);
+    console.log('https://nominatim.openstreetmap.org/search?q='+place+'&format=json&addressdetails=1$limit=10');
+    $.getJSON('https://nominatim.openstreetmap.org/search?q='+place+'&format=json&addressdetails=1&limit=10', function(data) {
+        var items = [];
+        
+
+        $.each( data, function(key, val) {
+            $(".list-result").append('<li class="list-group-item list-search-element" >' + val["display_name"] + "</li>" )
+          });
+    })
+}
--- newtravel.php
+++ newtravel.php
@@ -10,69 +10,77 @@
     <?php include_once "assets/navbar.php" ?>
 
     <!-- Content section-->
-    <section class="">
-        <div class="row py-1 d-flex">
-            <div class="col">
-                <div class="container-xl overflow-auto ">
-                    <label class="ft-1 fw-bolder">Itinerary</label>
-                    <div class="row">
-                        <form class="mx-1" role="search">
-                            <label class="">Add or remove Days</label>
-                            <button onclick="addDays(-1)" class="btn-secondary btn mx-1" type="button"> <span class="material-symbols-outlined"> do_not_disturb_on </span> </button>
-                            <button onclick="addDays(1)" class="btn-secondary btn mx-1" type="button"> <span class="material-symbols-outlined"> add_circle </span> </button>
-                        </form>
-                    </div>
-                    <div class="row">
-                        <label class="ft-2 text-center fw-bolder">Day 1</label>
-                        <ul id="sortable" class="list-group">
-
+    <section class="container py-5">
+        <div class="row">
+            <div class="col-sm-4 text-center">
+                <label class="display-5 fw-bolder py-5">Your Itinerary</label>
+                <div class="row">
+                    <form class="mx-1" role="search">
+                        <label class="">Add or remove Days</label>
+                        <button onclick="addItineraryDays(-1)" class="btn-secondary btn mx-1" type="button"> <span class="material-symbols-outlined"> do_not_disturb_on </span> </button>
+                        <button onclick="addItineraryDays(1)" class="btn-secondary btn mx-1" type="button"> <span class="material-symbols-outlined"> add_circle </span> </button>
+                    </form>
+                </div>
+                <div class="row py-3">
+                    <label class="ft-2 fw-bolder py-3">Day 1</label>
+                    <div class="container">
+                        <ul class="list-group list-group-numbered">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                            <li class="list-group-item">Item 4</li>
+                            <li class="list-group-item">Item 5</li>
+                            <li class="list-group-item">Item 6</li>
+                            <li class="list-group-item">Item 7</li>
+                            <li class="list-group-item">Item 8</li>
                         </ul>
                     </div>
-                    <div class="row py-1">
+                </div>
+                <div class="row py-3">
 
-                        <label class="ft-2 text-center fw-bolder">Day 2</label>
-                            <ul class="list-group list-group-numbered">
-                                <li class="list-group-item">Item 1</li>
-                                <li class="list-group-item">Item 2</li>
-                                <li class="list-group-item">Item 3</li>
-                                <li class="list-group-item">Item 4</li>
-                                <li class="list-group-item">Item 5</li>
-                                <li class="list-group-item">Item 6</li>
-                                <li class="list-group-item">Item 7</li>
-                                <li class="list-group-item">Item 8</li>
-                            </ul>
-                    </div>
-                    <div class="row py-1">
-                        <label class="ft-2 text-centerfw-bolder">Day 3</label>
-                        <ul class="list-group data-mdb-sortable">
-                            <li class="list-group-item">Luogo 1</li>
-                            <li class="list-group-item">Luogo 2</li>
-                            <li class="list-group-item">Luogo 3</li>
-                            <li class="list-group-item">Luogo 4</li>
-                            <li class="list-group-item">Luogo 5</li>
+                    <label class="ft-2 fw-bolder py-3">Day 2</label>
+                    <div class="container">
+                        <ul class="list-group list-group-numbered">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                            <li class="list-group-item">Item 4</li>
+                            <li class="list-group-item">Item 5</li>
+                            <li class="list-group-item">Item 6</li>
+                            <li class="list-group-item">Item 7</li>
+                            <li class="list-group-item">Item 8</li>
                         </ul>
                     </div>
-                    <div class="row py-1">
-                        <label class="ft-2 text-centerfw-bolder">Day 4</label>
-                        <ul class="list-group">
-                            <li class="list-group-item">Luogo 1</li>
-                            <li class="list-group-item">Luogo 2</li>
-                            <li class="list-group-item">Luogo 3</li>
-                            <li class="list-group-item">Luogo 4</li>
-                            <li class="list-group-item">Luogo 5</li>
-                    </div>
-                    </ul>
                 </div>
             </div>
-            <div class="col">
-                search bar
+            <div class="col-sm-4 text-center ">
+                <label class="display-5 fw-bolder py-5">Search a new place</label>
+                <div class="container py-3">
+                    <div class="input-group">
+                        <input type="search" onchange=findPlace() class="form-control rounded" placeholder="Search a new place" aria-label="Search" aria-describedby="search-addon" id="search-place" />
+                        <button type="button" onclick=findPlace() class="btn btn-outline-primary">search</button>
+                    </div>
+                </div>
+                <div class="container">
+                    <ul class="list-group list-result">
+                    </ul>
+
+                </div>
+
+
 
             </div>
-            <div class="col">
+            <div class="col-sm-4 text-left">
+                <label class="display-5 fw-bolder py-5">Place info</label>
+                <ul class="list-group">
+                    <li class="list-group-item">Place Name:</li>
+                    <li class="list-group-item">Tel Number:</li>
+                    <li class="list-group-item">Street address:</li>
+                </ul>
 
+                <div id="map"></div>
             </div>
         </div>
-
     </section>
 
     <?php include_once "assets/footer.html" ?>
